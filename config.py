311937b432904a1010d3d9e548ab9683f46cf6b7
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-08-15 22:00:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -143,21 +143,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/cfbdee8103102bcb2e5da.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/00360393a15daf7fc4e9d.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/48f39202823b358203234.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
+STATS_IMG_URL = " https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
+SPOTIFY_ALBUM_IMG_URL = " https://telegra.ph/file/a6caa0b016e33168ee0e4.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = " https://telegra.ph/file/a6caa0b016e33168ee0e4.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
