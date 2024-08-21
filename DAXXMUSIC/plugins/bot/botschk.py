9884a2ea10dbc68bde6ec0ecf7b3f6d4685b3693
sungjinw04 <abhishekpandit4224@gmail.com>
2024-08-21 20:45:59 +0530

    Update botschk.py

--- DAXXMUSIC/plugins/bot/botschk.py
+++ DAXXMUSIC/plugins/bot/botschk.py
@@ -26,7 +26,7 @@ from datetime import datetime
 userbot = Userbot()
 
 
-BOT_LIST = ["OwnergitBOT", "NexikoBot", "GitWizardBot", "stringseasonrobot", "LivioXBot"]
+BOT_LIST = ["LightYagamiXBot", "MITSURI_MUSICXBOT", "SOULSGUARDIANBOT"]
 
 @app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
 async def bots_chk(_, message):
