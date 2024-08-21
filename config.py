2d9d985f9c8cb9e6daa504b8e0befa9b6c72f179
sungjinw04 <abhishekpandit4224@gmail.com>
2024-08-21 20:58:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,26 +9,34 @@ from pyrogram import filters
 load_dotenv()
 # ------------------------------------
 # -----------------------------------------------------
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "25064357"))
+API_HASH = getenv("API_HASH", "cda9f1b3f9da4c0c93d1f5c23ccb19e2")
 
-EVAL = list(map(int, getenv("EVAL", "7453770651").split()))
-# ------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN")
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN", "7262095494:AAFclA3a3SDbyFkedrnQcg5g2lZF87vTUI8")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","tanjiro1249")
+OWNER_USERNAME = getenv("OWNER_USERNAME","sung_jinwo4")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "Hinata_X_Music_robot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "soulsupremebot")
 # --------------------------------------------------------
-BOT_NAME = getenv("BOT_NAME" , "Hinata")
+BOT_NAME = getenv("BOT_NAME" , "𝐒𝐎𝐔𝐋 𝐒𝐔𝐏𝐑𝐄𝐌𝐄『 魂 』")
 # ---------------------------------------------------------
-ASSUSERNAME = getenv("ASSUSERNAME" , "Assistantmusicbotr")
+ASSUSERNAME = getenv("ASSUSERNAME" , "soulmusicassistant")
 # ---------------------------------------------------------
-
+UPSTREAM_REPO = UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/sungjinw04/DAXXMUSIC",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+) 
+# Maximum Limit Allowed for users to save playlists on bot's server
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "3000"))
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://tanjiro1564:tanjiro1564@cluster0.pp5yz4e.mongodb.net/?retryWrites=true&w=majority")
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 
@@ -37,10 +45,10 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # ----------------------------------------------------------------
 
 # ----------------------------------------------------------------
-LOGGER_ID = int(getenv("LOGGER_ID", -1002237336934))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002125082441))
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-OWNER_ID = int(getenv("OWNER_ID", 7453770651))
+OWNER_ID = int(getenv("OWNER_ID", 1886390680))
 # -----------------------------------------------------------------
 # -----------------------------------------------------------------
 
@@ -53,14 +61,8 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
 # ----------------------------------------------------------------
-UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO",
-    "https://github.com/vikas-851/HINATA",
-)
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
-GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
-)  # ----------------------------------------------------------------
+
+#-----------------------------------------------------
 # -------------------------------------------------------------------
 # --------------------------------------------------------------------
 # --------------------------------------------------------------------
@@ -69,8 +71,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/luffyxnetwork")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/luffy_x_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SOULS_BORNX")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+H85eTC_5eMg4YTQ1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
@@ -116,7 +118,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "5242880000"))
 # ------------------------------------
 # ------------------------------------
 # ------------------------------------
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION1", "BQGDgkYACWIOgPXPtF3VwzLiwB5EQbgeXPl3tgLVeU0oT_GhT-QgXaaRq6-ypuCELOUlArfAGgQAKPMhgrpH_BhiSk_ZSVp50cPv_5i5giG6h1E67uiHAAKOMdkwOP4o90LZn7qXC4Rma69dCZAIqYZQH0Ognkmydta4WmeTJG9gpfL1v3kKnOIEVJchQjEeRge_eYV-G3ydrDrAjpyVM4cNm9w29QNVNSyHAuvEamKtRTsxL5joj9Tk3f5Hw5Lee55y2YVpVhbgyI7LTjeFf40EGaPh7WhwHUDY5iWeEO9VWpefDahBD01NW0JnmOQ3veoWqSKhoE2hGusPGb5VAfWHAiA_vAAAAAGPeoUyAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -143,21 +145,21 @@ confirmer = {}
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
+    "START_IMG_URL", "https://telegra.ph//file/919714d04904fae43ffd0.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
+    "PING_IMG_URL", "https://telegra.ph//file/466d670a36728a36283c9.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/3b8c930d4fa2e8da5111e.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d14bd448a2b51acbce05e.jpg"
-SPOTIFY_ALBUM_IMG_URL = " https://telegra.ph/file/a6caa0b016e33168ee0e4.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = " https://telegra.ph/file/a6caa0b016e33168ee0e4.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/f4395ca92239d6de6df8b.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/a44534c784f1eac464d85.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/d03b8bbec0af13849e031.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/5767b8e5619d616c7bf71.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/d83a2cf2bd0dd868f37ae.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/f4395ca92239d6de6df8b.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/35b034fe2cdc2effdd02e.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/45abf960687cff85f5855.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/eeff283b0ad70d40b80da.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/abdbf823aa2485c241b19.jpg"
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
