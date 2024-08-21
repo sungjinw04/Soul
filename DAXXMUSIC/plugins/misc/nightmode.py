bc3fad9e38fd638097cb21400634b17dd56f134e
sungjinw04 <abhishekpandit4224@gmail.com>
2024-08-21 21:04:32 +0530

    Update nightmode.py

--- DAXXMUSIC/plugins/misc/nightmode.py
+++ DAXXMUSIC/plugins/misc/nightmode.py
@@ -11,7 +11,6 @@ from DAXXMUSIC.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_n
 CLOSE_CHAT = ChatPermissions(
     can_send_messages=False,
     can_send_media_messages = False,
-    can_send_other_messages = False,
     can_send_polls = False,
     can_change_info = False,
     can_add_web_page_previews = False,
